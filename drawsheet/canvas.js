413b228dc2a8f729a0b177b4234aad766b6e80bb
ludvig <61148058+drtassen@users.noreply.github.com>
2021-08-26 19:23:42 +0200

    litt fremgang på 1 time

--- drawsheet/canvas.js
+++ drawsheet/canvas.js
@@ -1,1 +1,19 @@
 console.log('heio');
+
+window.addEventListener("load"), () => {
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getcontext("2d");
+
+    canvas.Height = window.innerHeight;
+    canvas.width = innerWidth;
+
+    ctx.fillrect(100, 100, 200, 500);
+
+
+}
+let pg;
+
+function setup() {
+    createCanvas(100, 100);
+    pg = createGraphics(100, 100);
+}
